86fe2c829969f8100086b944840b02b68fa9a711
Akul Singhal <singhal.akul9@gmail.com>
2021-03-08 18:46:31 +0530

    add optional mod parameter to mod expo

--- math/mod_expo.cpp
+++ math/mod_expo.cpp
@@ -1,13 +1,13 @@
 // Function to calculate (x ^ p) % MOD in O(log p)
 // Limits: x -> [INT64_MIN, INT64_MAX], p -> [0, INT64_MAX]
 
-int mod_expo(int x, int p) {
+int mod_expo(int x, int p, int mod = MOD) {
     int res = 1;
-    x = ((x % MOD) + MOD) % MOD;
+    x = ((x % mod) + mod) % mod;
     while (p > 0) {
         if (p & 1)
-            res = (res * x) % MOD;
-        x = (x * x) % MOD;
+            res = (res * x) % mod;
+        x = (x * x) % mod;
         p /= 2;
     }
     return res;
@@ -17,6 +17,6 @@ int mod_expo(int x, int p) {
 // Requirements: MOD is prime
 // Limits: x -> [INT64_MIN, INT64_MAX]
 
-int mod_inv(int x) {
-    return mod_expo(x, MOD - 2);
+int mod_inv(int x, int mod = MOD) {
+    return mod_expo(x, mod - 2, mod);
 }
